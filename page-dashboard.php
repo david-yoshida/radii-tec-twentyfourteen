39bde83c522dd8db93bc485203e990e2d9bbe116
David Yoshida <david.yoshida@yoshweb.com>
2015-07-15 15:44:34 -0400

    Dashboard updates

--- page-dashboard.php
+++ page-dashboard.php
@@ -47,9 +47,122 @@ get_header(); ?>
 
 			} );
 
+		}
 
+
+		// Function copied from TEC /plugins/events-calendar-pro/src/resources/js/widget-calendar.js?ver=3.10
+		function fix_widget_height() {
+
+			var wrapper = $( '.tribe-mini-calendar-wrapper' );
+			if ( $( '.tribe-mini-calendar-wrapper.layout-wide' ).length && $( '.tribe-mini-calendar-right' ).length ) {
+				var right_bar = $( '.tribe-mini-calendar-right' );
+				var w_height = wrapper.height();
+				var rb_height = right_bar.outerHeight();
+				rb_height = rb_height + 20;
+
+				if ( rb_height > w_height ) {
+					wrapper.css( 'height', rb_height + 'px' );
+				}
+				else {
+					wrapper.css( 'height', 'auto' );
+				}
+			}
+			else if ( $( '.tribe-mini-calendar-wrapper.layout-wide' ).length ) {
+				wrapper.css( 'height', 'auto' );
+			}
 
 		}
+		
+		// Checkbox Logic
+	   	$('.TEC-Widget-filter').change(function() {
+
+
+	   		// Get the Event Type checkbox values
+	   		var myTECIDs = [];  // init the new filter
+
+			$(".TEC-Widget-filter").each(function(){
+
+				if ($(this).is(':checked')){  					
+				    	myTECIDs.push($(this).val());
+				    }
+			});
+
+
+
+			var myRegionIDs = [];
+			extraText = '';
+			// Get the Event Region dropdown values
+			myRegionIDs = $('#TEC-Widget-filter-Region').val();
+
+			if(myRegionIDs != ''){
+				extraText = ',{"taxonomy":"tribe_events_cat","field":"id","operator":"IN","terms":["' + myRegionIDs + '"]}';
+			}
+
+			
+
+			// Override intial data-tax-query  attribute
+			overRideTaxonomy = '[{"taxonomy":"tribe_events_cat","field":"id","operator":"IN","terms":[' + myTECIDs.toString() + ']} ' + extraText + ']';
+			$(".tribe-mini-calendar").attr("data-tax-query",overRideTaxonomy);
+
+   			// Function copied from TEC /plugins/events-calendar-pro/src/resources/js/widget-calendar.js?ver=3.10
+			var date = new Date(),
+				day = date.getDate().toString(),
+				month = (date.getMonth() + 1).toString(),
+				year = date.getFullYear().toString(),
+				current = year + '-' + (month[1] ? month : '0' + month[0]) + '-' + (day[1] ? day : '0' + day[0]),
+				current_ym = current.slice( 0, 7 );
+
+			var $this = $( this );
+
+			//var $current_calendar = $this.closest( '.tribe-mini-calendar' );
+			var $current_calendar = $( '.tribe-mini-calendar' );  // works
+			//var $current_calendar_wrapper = $this.closest( '.tribe-mini-calendar-wrapper' );
+			var $current_calendar_wrapper = $( '.tribe-mini-calendar-wrapper' ); // works
+			
+
+			//tempDateString = $('.tribe-mini-calendar-nav-link').attr("data-month");
+			tempCurrentDateString = $('.tribe-mini-calendar').attr("data-eventdate");
+			tempCurrentDateString = tempCurrentDateString.slice(0,10);  // only grab 2015-09-18
+
+			//alert(tempCurrentDateString);  // Debug
+
+			var month_target = tempCurrentDateString,
+				target_ym = month_target.slice( 0, 7 );
+
+			if ( current_ym == target_ym ) {
+				month_target = current;
+			}
+
+			var params = {
+				action   : 'tribe-mini-cal',
+				eventDate: month_target,
+				count    : $current_calendar.data( 'count' ),
+				tax_query: $current_calendar.data( 'tax-query' ),
+				nonce    : $current_calendar.data( 'nonce' )
+			};
+			$current_calendar.find( '.tribe-mini-calendar-nav div > span' ).addClass( 'active' ).siblings( '#ajax-loading-mini' ).show();
+
+			$.post(
+				TribeMiniCalendar.ajaxurl,
+				params,
+				function( response ) {
+					$current_calendar.find( '.tribe-mini-calendar-list-wrapper' ).remove();
+					if ( response.success ) {
+						var $the_content = $.parseHTML( response.html );
+						$current_calendar.find( '.tribe-mini-calendar-nav div > span' ).removeClass( 'active' ).siblings( '#ajax-loading-mini' ).hide();
+						$current_calendar_wrapper.replaceWith( $the_content );
+						fix_widget_height();
+					}
+				}
+			);
+
+
+
+
+      
+	    });
+
+
 
 	});
 
@@ -97,7 +210,7 @@ get_header(); ?>
 
 				//if (isset($_GET['saved']))
 
-				if(!$_SESSION["radii-tec-filter"]){
+				if(isset($_SESSION["radii-tec-filter"])){
 
 					 $_SESSION["radii-tec-filter"]= $RAD_CAT_SLUG;
 				}
@@ -160,21 +273,41 @@ get_header(); ?>
 						<div id="panel-tec-calendar-view" >
 						<h2>Calendar View</h2>
 
+						Event Type:
+						<ul>
+						<li><input class="TEC-Widget-filter" type="checkbox" value="20" checked>GP Summit</li>
+						<li><input class="TEC-Widget-filter" type="checkbox" value="4" checked>Industry Events</li>
+						<li><input class="TEC-Widget-filter" type="checkbox" value="22" checked>Institute</li>
+						<li><input class="TEC-Widget-filter" type="checkbox" value="5" checked>Members' Only</li>
+						<li><input class="TEC-Widget-filter" type="checkbox" value="3" checked>Webcast Series</li>
+						<li><input class="TEC-Widget-filter" type="checkbox" value="21" checked>Workshop</li>
+						</ul>
+						
 
-						<div class="dropdown">
-						  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-						    Dropdown
-						    <span class="caret"></span>
-						  </button>
-						  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-						    <li><a href="/dashboard/?RAD_VIEW=calendar&RAD_CAT_SLUG=gp-summit">GP Summit</a></li>
-						    <li><a href="/dashboard/?RAD_VIEW=calendar&RAD_CAT_SLUG=industry-events">Industry Events</a></li>
-						    <li><a href="/dashboard/?RAD_VIEW=calendar&RAD_CAT_SLUG=institute">Institute</a></li>
-						    <li><a href="/dashboard/?RAD_VIEW=calendar&RAD_CAT_SLUG=members-only-conferences">Members' Only</a></li>
-						    <li><a href="/dashboard/?RAD_VIEW=calendar&RAD_CAT_SLUG=webcast-series">Webcast Series</a></li>
-						    <li><a href="/dashboard/?RAD_VIEW=calendar&RAD_CAT_SLUG=workshop">Workshop</a></li>
-						  </ul>
-						</div>
+						Region:
+						<select id="TEC-Widget-filter-Region" class="TEC-Widget-filter">
+							<option value="">Region</option>
+							<option value="11">Africa</option>
+							<option value="12">Asia</option>
+							<option value="13">Europe</option>
+							<option value="14">Middle East</option>
+							<option value="15">North America</option>
+							<option value="16">Oceania</option>
+							<option value="17">South America</option>
+						</select>
+
+						<p>&nbsp;</p>
+						<!---
+						<ul>
+						<li><input class="TEC-Widget-filter-region" type="checkbox" value="11" checked>Africa</li>
+						<li><input class="TEC-Widget-filter-region" type="checkbox" value="12" checked>Asia</li>
+						<li><input class="TEC-Widget-filter-region" type="checkbox" value="13" checked>Europe</li>
+						<li><input class="TEC-Widget-filter-region" type="checkbox" value="14" checked>Middle East</li>
+						<li><input class="TEC-Widget-filter-region" type="checkbox" value="15" checked>North America</li>
+						<li><input class="TEC-Widget-filter-region" type="checkbox" value="16" checked>Oceania</li>
+						<li><input class="TEC-Widget-filter-region" type="checkbox" value="21" checked>South America</li>
+						</ul>
+						--->
 
 
 						<?php
@@ -184,6 +317,10 @@ get_header(); ?>
 								$str = '[tribe_mini_calendar category="'. $RAD_CAT_SLUG .'"]';
 							else
 								$str = '[tribe_mini_calendar category="industry-events"]';
+
+							//$str = '[tribe_mini_calendar category="workshop,industry-events"]';
+							//$str = '[tribe_mini_calendar category="gp-summit,workshop"]';
+							$str = '[tribe_mini_calendar category="gp-summit,industry-events,institute,members-only-conferences,webcast-series,workshop,africa,asia,europe,middle-east,north-america,oceania,south-america"]';
 
 
 
